0c502f11faf73544fa9ce4998e283b676743e425
Yasser A. Gamal <63634166+yasser-skiller@users.noreply.github.com>
2022-10-06 16:25:26 +0200

    front

---
+++ components/CoursesPage/FilesSec.vue
@@ -0,0 +1,105 @@
+<template>
+  <div>
+    <b-row align-h="center"  class="flex-wrap">
+      <b-col cols="12"  lg="3" class="px-2 my-3">
+        <div class="bg py-2 px-3 rounded_1">
+          <div class="d-flex">
+            <img :src="require(`~/assets/icon/Group 2286.svg`)"  class="ml-2" alt="icon"/>
+            <div>
+              <h6 class="font-weight-bold">ملف محلول</h6>
+              <p>ملف امتحان محلول 15 سؤال</p>
+            </div>
+          </div>
+          <div class="d-flex mx-auto mt-5 justify-content-center ">
+            <img :src="require(`~/assets/icon/pdf0.svg`)"  class="ml-2" alt="icon"/>
+            <p class="my-0">ملف بي دي اف 4 ميجا</p>
+          </div>
+
+          <div class="d-flex mt-4 justify-content-center ">
+            <b-button size="sm" class="btn btn_Dark py-2 px-5  rounded_0" type="button" > تحميل الملف</b-button>
+
+          </div>
+        </div>
+
+      </b-col>
+      <b-col cols="12"  lg="3" class="px-2 my-3">
+        <div class="bg py-2 px-3 rounded_1">
+          <div class="d-flex">
+            <img :src="require(`~/assets/icon/Group 2286.svg`)"  class="ml-2" alt="icon"/>
+            <div>
+              <h6 class="font-weight-bold">ملف محلول</h6>
+              <p>ملف امتحان محلول 15 سؤال</p>
+            </div>
+          </div>
+          <div class="d-flex mx-auto mt-5 justify-content-center ">
+            <img :src="require(`~/assets/icon/pdf0.svg`)"  class="ml-2" alt="icon"/>
+            <p class="my-0">ملف بي دي اف 4 ميجا</p>
+          </div>
+
+          <div class="d-flex mt-4 justify-content-center ">
+            <b-button size="sm" class="btn btn_Dark py-2 px-5  rounded_0" type="button" > تحميل الملف</b-button>
+
+          </div>
+        </div>
+
+      </b-col>
+      <b-col cols="12"  lg="3" class="px-2 my-3">
+        <div class="bg py-2 px-3 rounded_1">
+          <div class="d-flex">
+            <img :src="require(`~/assets/icon/Group 2286.svg`)"  class="ml-2" alt="icon"/>
+            <div>
+              <h6 class="font-weight-bold">ملف محلول</h6>
+              <p>ملف امتحان محلول 15 سؤال</p>
+            </div>
+          </div>
+          <div class="d-flex mx-auto mt-5 justify-content-center ">
+            <img :src="require(`~/assets/icon/pdf0.svg`)"  class="ml-2" alt="icon"/>
+            <p class="my-0">ملف بي دي اف 4 ميجا</p>
+          </div>
+
+          <div class="d-flex mt-4 justify-content-center ">
+            <b-button size="sm" class="btn btn_Dark py-2 px-5  rounded_0" type="button" > تحميل الملف</b-button>
+
+          </div>
+        </div>
+
+      </b-col>
+      <b-col cols="12"  lg="3" class="px-2 my-3">
+        <div class="bg py-2 px-3 rounded_1">
+          <div class="d-flex">
+            <img :src="require(`~/assets/icon/Group 2286.svg`)"  class="ml-2" alt="icon"/>
+            <div>
+              <h6 class="font-weight-bold">ملف محلول</h6>
+              <p>ملف امتحان محلول 15 سؤال</p>
+            </div>
+          </div>
+          <div class="d-flex mx-auto mt-5 justify-content-center ">
+            <img :src="require(`~/assets/icon/pdf0.svg`)"  class="ml-2" alt="icon"/>
+            <p class="my-0">ملف بي دي اف 4 ميجا</p>
+          </div>
+
+          <div class="d-flex mt-4 justify-content-center ">
+            <b-button size="sm" class="btn btn_Dark py-2 px-5  rounded_0" type="button" > تحميل الملف</b-button>
+
+          </div>
+        </div>
+
+      </b-col>
+
+    </b-row>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+  .bg{
+    background-color: #F6F4F4;
+  }
+
+</style>
---
+++ components/CoursesPage/TaskSec.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="d-flex flex-wrap justify-content-between align-items-center bg p-3">
+    <img :src="require(`~/assets/icon/Group 2284.svg`)"  class="" alt="icon"/>
+
+    <div class="rig pl-3">
+      <h6 class="text_dark font-weight-bold">امتحان الكتروني اونلاين</h6>
+      <p>امتحان من 25 سؤال</p>
+    </div>
+    <p class="w-50">
+      هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص العربى، حيث يمكنك أن تولد مثل هذا النص
+    </p>
+
+    <b-button class="btn btn_orange rounded_0 px-5 py-3 "> ابدا الامتحان الان</b-button>
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+  .bg{
+    background-color: #F6F4F4;
+    border-radius: 20px;
+  }
+  .rig{
+    border-left: 1px solid #8E8F9B;
+  }
+</style>
